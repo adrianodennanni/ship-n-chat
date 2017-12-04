d7d6dafc1488a76fdd08ac2d2c2fbdc71866ae33
Luiz Fernando P. L. Cabral <luizfercabp@gmail.com>
2017-12-04 18:44:05 -0200

    retirado preventDefault das keys

--- pages/client/client.js
+++ pages/client/client.js
@@ -8,12 +8,12 @@
 
   $( document ).ready(function() {
 
-    window.addEventListener("keydown", function(e) {
+    /*window.addEventListener("keydown", function(e) {
         // space and arrow keys
         if([32, 37, 38, 39, 40].indexOf(e.keyCode) > -1) {
             e.preventDefault();
         }
-    }, false);
+    }, false);*/
 
     SERVER_ADDRESS = document.URL;
 
